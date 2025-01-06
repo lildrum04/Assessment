c23dd520e3cb939adf5aae86538d4077effcec93
LilDrum33 <iftodimarian86@gmail.com>
2025-01-06 16:05:38 +0000

    save

---
+++ classes.py
@@ -0,0 +1,26 @@
+characters = {
+    "Archer": {
+        "name": "Artemis",
+        "HP": 50,
+        "AC": 14,
+        "Attack_range": "20ft", #Archer has 20ft attack range
+        "Damage_range": (1,8), # Archer uses 1d8 for attack damage
+        "Special_ability": "Rapid Shot"
+    },
+    "Wizard": {
+        "name": "Eviar",
+        "HP": 40,
+        "AC": 12,
+        "Attack_range": "25ft", # Wizard has 25ft attack range
+        "Damage_range": (1,10), # Wizard uses 1d10 for attack damage
+        "Special_ability": "Mana Shield"
+    },
+    "Barbarian": {
+        "name": "Yudrom",
+        "HP": 100,
+        "AC": 16,
+        "Attack_range": "25ft", # Wizard has 25ft attack range
+        "Damage_range": (1,10), # Wizard uses 1d10 for attack damage
+        "Special_ability": "Mana Shield"
+    }
+}
---
+++ enemies.py
@@ -0,0 +1,23 @@
+
+
+enemies = {
+    "Goblin": {
+         "AC": 12, # Armour Class
+         "HP": 15, # Hit Points
+         "Attack": [1,6] # Damage range for attacks (1d6)
+    },
+    "Skeleton": {
+        "AC": 10, # Armour Class
+         "HP": 20, # Hit Points
+         "Attack": [1,8] # Damage range for attacks (1d8)
+    },
+    "Orc": {
+        "AC": 14, # Armour Class
+        "HP": 35, # Hit Points
+        "Attack": [2,10] # Damage range for attacks (1d10)
+    }
+}
+print(enemies["Goblin"])
+goblin_stats = enemies["Goblin"]
+for stat, value in goblin_stats.items():
+    print(f"Goblin's {stat}: {value}")
